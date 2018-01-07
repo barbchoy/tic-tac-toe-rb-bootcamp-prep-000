f4f6483f90567fc71be97acf5b596b231f54d016
Barbara Choy <barbchoy@gmail.com>
2018-01-07 05:42:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -87,3 +87,22 @@ end
 def winner(board)
   won?(board)? board[won?(board)[0]] : nil
 end
+
+def turn_count(board)
+  count = 0
+  board.each do |space|
+    if space != " " && space != ""
+      count += 1
+    end
+  end
+  count
+end
+
+def current_player(board)
+  count = turn_count(board)
+  if count % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
